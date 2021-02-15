43676b633d18a25b7bbc906be26a4497ec467207
Javier López de Ancos <javier@zartis.com>
2021-02-15 16:58:22 +0100

    fix: remove reatc dependency from hooks

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,5 +1,3 @@
-import { useMemo } from 'react'
-
 import {
   ClassProperties,
   getClass,
@@ -7,11 +5,8 @@ import {
   getTestId,
 } from '../native'
 
-export const useGetClass = (componentName: string, classProperties?: ClassProperties): string =>
-  useMemo(() => getClass(componentName, classProperties), [])
+export const useGetClass = (componentName: string, classProperties?: ClassProperties): string => getClass(componentName, classProperties)
 
-export const useGetId = (componentName: string, componentId?: string): string =>
-  useMemo(() => getId(componentName, componentId), [])
+export const useGetId = (componentName: string, componentId?: string): string => getId(componentName, componentId)
 
-export const useGetTestId = (componentName: string, componentTestId?: string): string =>
-  useMemo(() => getTestId(componentName, componentTestId), [])
+export const useGetTestId = (componentName: string, componentTestId?: string): string => getTestId(componentName, componentTestId)
